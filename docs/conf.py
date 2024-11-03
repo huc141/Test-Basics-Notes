7368c2f18a09bd201070c7811871694556393a41
huc141 <1349983371@qq.com>
2024-11-03 16:35:44 +0800

    调试

--- docs/conf.py
+++ docs/conf.py
@@ -7,14 +7,14 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'test'
-copyright = '2024, testna,e'
-author = 'testna,e'
+copyright = '2024, testnae'
+author = 'testnae'
 release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark','sphinx_markdown_tables']
+extensions = ['recommonmark', 'sphinx_markdown_tables']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +24,9 @@ language = 'zh_CN'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+import sphinx_rtd_theme
+
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# html_static_path = ['_static']
